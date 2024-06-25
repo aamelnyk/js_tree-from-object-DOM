7637eb4b3d20304a8932014abf8cf37a0383bbc4
Melnyk Anna <anetamelnyk25@gmail.com>
2024-06-25 18:13:44 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,39 @@
+"use strict";
+const food = {
+    Drink: {
+        Wine: {},
+        Schnaps: {}
+    },
+    Fruit: {
+        Red: {
+            Cherry: {},
+            Strawberry: {}
+        },
+        Yellow: {
+            Banana: {},
+            Pineapple: {}
+        }
+    }
+};
+const tree = document.querySelector("#tree");
+function createUl(item) {
+    return document.createElement("ul");
+}
+function createLi(item) {
+    const li = document.createElement("li");
+    li.textContent = item;
+    return li;
+}
+function createTree(element, data) {
+    const ul = createUl();
+    const arr = Object.entries(data);
+    arr.forEach(([key, value])=>{
+        const li = createLi(key);
+        ul.appendChild(li);
+        if (Object.keys(value).length > 0) createTree(li, value);
+    });
+    element.appendChild(ul);
+}
+createTree(tree, food);
+
+//# sourceMappingURL=index.f75de5e1.js.map
